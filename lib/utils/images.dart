192248e7fde7688f6497b1cf26c9915447553729
Mayank Variya <103999830+MayankVariya@users.noreply.github.com>
2023-04-22 18:00:18 +0530

    done

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,4 +1,4 @@
 const String logo = "assets/images/logo.jpg";
 const String splash = "assets/images/splash.jpg";
 
-const String appUrl="";
+const String appUrl="https://drive.google.com/file/d/1ErKHZeSJFxk07XrKRoUlf3oTTJG0lKxI/view?usp=share_link";
